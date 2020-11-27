b4b1c4b2d3f12e7efb012055c8631d9c8f7172a3
Chad Parker <chad@chadparker.com>
2020-11-27 09:18:54 -0800

    Save following words into hash table

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -7,6 +7,12 @@ with open("input.txt") as f:
 # TODO: analyze which words can follow other words
 # Your code here
 
+hash_table = {}
+for index, word in enumerate(words):
+    following_words = hash_table.get(word, set())
+    if index < len(words)-1:
+        following_words.add(words[index+1])
+        hash_table[word] = following_words
 
 # TODO: construct 5 random sentences
 # Your code here
