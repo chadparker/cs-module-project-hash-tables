5a4889f671ac202356151d38b119ae9f5dd764a1
Chad Parker <chad@chadparker.com>
2020-11-28 20:14:56 -0800

    Remove ignored characters from text

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,8 +1,14 @@
 
+ignored_str = '":;,.-+=/\|[]{}()*^&'
+ignored_chars = set()
+for char in ignored_str:
+    ignored_chars.add(char)
+
 with open("robin.txt") as f:
     text = f.read()
 
 text = text.lower()
+text = ''.join(c for c in text if c not in ignored_chars)
 words = text.split()
 
 for word in words:
