0786d5a56cf46100a0589d0140766bf3b7561bdb
Chad Parker <chad@chadparker.com>
2021-01-08 13:43:05 -0800

    Find longest word length while looping

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -11,8 +11,11 @@ text = text.lower()
 text = ''.join(c for c in text if c not in ignored_chars)
 words = text.split()
 
+longest_word_length = 0
 hash_table = {}
 for word in words:
     count = hash_table.get(word, 0)
     count -= 1  # store count as negative for descending sort
     hash_table[word] = count
+    if len(word) > longest_word_length:
+        longest_word_length = len(word)
