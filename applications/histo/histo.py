676e52ce9f183dc4814e5df4805e87b4d9c1830c
Chad Parker <chad@chadparker.com>
2021-01-08 13:45:21 -0800

    Print words in correct format

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -22,3 +22,10 @@ for word in words:
         longest_word_length = len(word)
 
 items = sorted(hash_table.items(), key=itemgetter(1, 0))
+for item in items:
+    word = item[0]
+    num_spaces = longest_word_length - len(word) + 2
+    spaces = " " * num_spaces
+    count = -item[1]
+    hashmarks = "#" * count
+    print(f"{word}{spaces}{hashmarks}")
