1ddca892aafa00a3cfa2eec07433926deae06517
Chad Parker <chad@chadparker.com>
2021-01-09 15:05:18 -0800

    Sort letters by count

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -1,6 +1,7 @@
 # Use frequency analysis to find the key to ciphertext.txt, and then
 # decode it.
 from collections import defaultdict
+from operator import itemgetter
 
 letters_sortedby_most_used = [
     'E', 'T', 'A', 'O', 'H', 'N', 'R', 'I', 'S', 'D', 'L', 'W', 'U',
@@ -18,3 +19,8 @@ letter_counts = defaultdict(int)
 for char in cipher_text:
     if char in letters:
         letter_counts[char] += 1
+
+letter_counts_sorted = sorted(letter_counts.items(), key=itemgetter(1), reverse=True)
+letters_sorted = []
+for pair in letter_counts_sorted:
+    letters_sorted.append(pair[0])
