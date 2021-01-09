476c88c7a0fba1bd5ec0c731388362168cc1989f
Chad Parker <chad@chadparker.com>
2021-01-09 15:03:13 -0800

    Create set for quicker lookups

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -6,5 +6,9 @@ letters_sortedby_most_used = [
     'G', 'F', 'B', 'M', 'Y', 'C', 'P', 'K', 'V', 'Q', 'J', 'X', 'Z'
 ]
 
+letters = set()
+for letter in letters_sortedby_most_used:
+    letters.add(letter)
+
 with open("ciphertext.txt") as f:
     cipher_text = f.read()
